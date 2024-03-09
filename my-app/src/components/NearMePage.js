5fd71c8035e0b02de6cdb8497c474977b05120b2
yuximing <93099324+yuximing@users.noreply.github.com>
2024-03-09 16:20:22 -0500

    added markers on the map

--- my-app/src/components/NearMePage.js
+++ my-app/src/components/NearMePage.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import 'leaflet/dist/leaflet.css';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Icon } from "leaflet";
 
 const NearMePage = () => {
+    const markers = [
+        {
+            geocode: [48.86, 2.3522],
+            popUp: "Hello, I am pop up 1"
+        },
+        {
+            geocode: [48.85, 2.3522],
+            popUp: "Hello, I am pop up 2"
+        },
+        {
+            geocode: [48.855, 2.34],
+            popUp: "Hello, I am pop up 3"
+        }
+    ];
+
+    const customIcon = new Icon({
+        iconUrl: require("./../icons/pin.png"),
+        iconSize: [38, 38] // size of the icon
+    });
+
   return (
     <div className='flex h-screen w-screen'>
       <MapContainer
@@ -14,6 +35,11 @@ const NearMePage = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
+        {markers.map((marker) => (
+          <Marker position={marker.geocode} icon={customIcon}>
+            <Popup>{marker.popUp}</Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
