a1a38f30382af53a0d086134f08d40a90f2dad78
yuximing <93099324+yuximing@users.noreply.github.com>
2024-03-09 15:58:02 -0500

    rendered map

--- my-app/src/components/NearMePage.js
+++ my-app/src/components/NearMePage.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
+import 'leaflet/dist/leaflet.css';
+import { MapContainer, TileLayer } from 'react-leaflet';
 
 const NearMePage = () => {
   return (
-    <div>
-        hello
+    <div className='flex h-screen w-screen'>
+      <MapContainer
+        className='h-screen w-screen'
+        center={[48.8566, 2.3522]}
+        zoom={13}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+      </MapContainer>
     </div>
-  )
-}
+  );
+};
 
-export default NearMePage
+export default NearMePage;
