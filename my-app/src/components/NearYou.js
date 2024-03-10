40c2cd53d083f07f81d49e84a494815267d9578e
AnonymousKitty <theonlycat2015@gmail.com>
2024-03-09 21:38:17 -0500

    Implemented List Items as buttons which highlight when hovered over and expands when clicked

--- my-app/src/components/NearYou.js
+++ my-app/src/components/NearYou.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './NearYou.css'; // Import the CSS for styling
 import WebFont from 'webfontloader';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 WebFont.load({
     google: {
@@ -9,11 +10,21 @@ WebFont.load({
     }
 });
 
-function dododo() {
-    var listContainer = document.getElementById('list-container');
-    listContainer.classList.toggle('viewing');
-        
+
+const ListItem = ({ foodItems, restaurantName, address }) => {
+    const [visible, setVisible] = useState(false)
+    return (
+        <div className={visible ? "listElementActive" : "listElementHover"} onClick={() => {setVisible(!visible)}}>
+            <h4>{restaurantName}</h4>
+            <h5>{address}</h5>
+            <ul className={visible ? 'show':'hide'}>
+                {foodItems.map(food => <li>{food}</li>)}
+            </ul>
+        </div>
+    )
 }
+
+
 
 const NearYou = () => {
     const navigate = useNavigate();
@@ -30,9 +41,8 @@ const NearYou = () => {
                     <h3 className='subtitle'>Locations serving food near you:</h3>
                 </div>
                 <ul id='list-container'>
-                    <li onClick={dododo}>Task 1</li>
-                    <li>Task 2</li>
-                    <li>Task 3</li>
+                    <ListItem foodItems = {['apple', 'banana']} restaurantName = 'McDs' address = '1234 princess st.'></ListItem>
+                    <ListItem foodItems = {['apple', 'banana']} restaurantName = 'McDs' address = '1234 princess st.'></ListItem>
                 </ul>
             </div>
         </div>
